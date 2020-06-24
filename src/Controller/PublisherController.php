9f567bf399779c9465974b0a4fdfccc5cb88d771
tsouhaieb <tarhounisouhaieb@gmail.com>
2020-06-24 09:38:06 +0100

    fix error after installing profiler

--- src/Controller/PublisherController.php
+++ src/Controller/PublisherController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mercure\Publisher;
+use Symfony\Component\Mercure\PublisherInterface;
 use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +18,7 @@ class PublisherController extends AbstractController
     /**
      * @Route("/publish/{topic}", name="publisher", methods={"POST"})
      */
-    public function index(Publisher $publisher, $topic, Request $request)
+    public function index(PublisherInterface $publisher, $topic, Request $request)
     {
         $publisher(new Update($topic, $request->getContent()));
         return new Response('success');
