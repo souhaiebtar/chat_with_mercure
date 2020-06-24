b882e664c05b0bd397ff63502b64e19a0d3e26af
tsouhaieb <tarhounisouhaieb@gmail.com>
2020-06-24 14:52:56 +0100

    add authentication using HTTP `Authorization` header

--- ReadMe.md
+++ ReadMe.md
@@ -4,7 +4,7 @@ start by cloning the repo than
 
 1. Start mercure using this command (docker used):
     ```BASH
-    docker run -e JWT_KEY='!ChangeMe!' -e ALLOW_ANONYMOUS=1 \
+    docker run -e JWT_KEY='!ChangeMe!' \
      -e CORS_ALLOWED_ORIGINS="*" -e PUBLISH_ALLOWED_ORIGINS='*' \
      -e DEBUG=1 -p 9090:80 dunglas/mercure:v0.10.2
     ```
@@ -14,7 +14,7 @@ start by cloning the repo than
     php -S localhost:8000 -t public
     ``` 
 
-3. Open the chat url `http://localhost:8000/chat` in two different browser
+3. Open the chat url `http://localhost:8000/chat` in two different browsers
 
 ## original code [1][1]
 
--- src/Controller/PublisherController.php
+++ src/Controller/PublisherController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mercure\PublisherInterface;
+use Symfony\Component\Mercure\Publisher;
 use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +17,14 @@ class PublisherController extends AbstractController
 {
     /**
      * @Route("/publish/{topic}", name="publisher", methods={"POST"})
+     * @param Publisher $publisher
+     * @param $topic
+     * @param Request $request
+     * @return Response
      */
-    public function index(PublisherInterface $publisher, $topic, Request $request)
+    public function index(Publisher $publisher, $topic, Request $request)
     {
-        $publisher(new Update($topic, $request->getContent()));
+        $publisher(new Update($topic, $request->getContent(), true));
         return new Response('success');
     }
 }
